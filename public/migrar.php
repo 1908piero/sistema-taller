353b9fb7620aaac99e2e9b87cd0ba644ac0300d9
1908piero <yhojan.piero18@gmail.com>
2026-07-15 18:34:04 -0500

    fix: ruta autoload en migrar.php

--- public/migrar.php
+++ public/migrar.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 $db = (new Config\Database())->getConnection();
 if (!$db) { die("Error BD"); }
 
